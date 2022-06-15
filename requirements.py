04af2b5993fcb7f01bba2c81af8683f9cc496a6b
47hx1-53r <99020554+47hxl-53r@users.noreply.github.com>
2022-06-15 13:09:04 -0500

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,34 +1,8 @@
-import os, time
-print("PLEASE WAIT WHILE DOWNLOADING THE REQUIREMENTS...\n")
-
-# REQUESTS
-os.system('clear')
-print("Installing requests")
-os.system('pip install requests')
-
-# COLORAMA
-os.system('clear')
-print("Installing colorama")
-os.system("pip install colorama")
-
-# DNSPYTHON
-os.system('clear')
-print('Installing dnspython')
-os.system('pip install dnspython')
-
-# PYFIGLET
-os.system('clear')
-print('Installing pyfiglet')
-os.system('pip install pyfiglet')
-
-#TQDM
-os.system('clear')
-print('Installing additional packages...')
-os.system('pip install tqdm')
-
-# SELF-KILLING
-os.system('clear')
-os.system('THIS FILE IS GOING TO SELF-DELETE IN 3 SECONDS...')
-time.sleep(3)
-os.system('clear')
-os.system('rm -rf requirements.py')
+import base64, codecs
+magic = 'aW1wb3J0IG9zLCB0aW1lCnByaW50KCJQTEVBU0UgV0FJVCBXSElMRSBET1dOTE9BRElORyBUSEUgUkVRVUlSRU1FTlRTLi4uXG4iKQoKIyBSRVFVRVNUUwpvcy5zeXN0ZW0oJ2NsZWFyJykKcHJpbnQoIkluc3RhbGxpbmcgcmVxdWVzdHMiKQpvcy5zeXN0ZW0oJ3BpcCBpbnN0YWxsIHJlcXVlc3RzJykKCiMgQ09MT1JBT'
+love = 'HRXo3Zhp3ymqTIgXPqwoTIupvpcPaOlnJ50XPWWoaA0LJkfnJ5aVTAioT9lLJ1uVvxXo3Zhp3ymqTIgXPWjnKNtnJ5mqTSfoPOwo2kipzSgLFVcPtbwVREBH1OMIRuCGtcipl5mrKA0MJ0bW2AfMJSlWlxXpUWcoaDbW0yhp3EuoTkcozptMT5mpUy0nT9hWlxXo3Zhp3ymqTIgXPqjnKNtnJ5mqTSfoPOxoaAjrKEbo24aXD'
+god = 'oKIyBQWUZJR0xFVApvcy5zeXN0ZW0oJ2NsZWFyJykKcHJpbnQoJ0luc3RhbGxpbmcgcHlmaWdsZXQnKQpvcy5zeXN0ZW0oJ3BpcCBpbnN0YWxsIHB5ZmlnbGV0JykKCiNUUURNCm9zLnN5c3RlbSgnY2xlYXInKQpwcmludCgnSW5zdGFsbGluZyBhZGRpdGlvbmFsIHBhY2thZ2VzLi4uJykKb3Muc3lzdGVtKCdwaXAgaW5'
+destiny = 'mqTSfoPO0pJEgWlxXPvZtH0IZEv1YFHkZFH5UPz9mYaA5p3EyoFtaL2kyLKVaXDcipl5mrKA0MJ0bW1EVFIZtExyZEFOWHlOUG0yBElOHGlOGEHkTYHESGRIHEFOWGvNmVSASD09BESZhYv4aXDc0nJ1yYaAfMJIjXQZcPz9mYaA5p3EyoFtaL2kyLKVaXDcipl5mrKA0MJ0bW3WgVP1lMvOlMKS1nKWyoJIhqUZhpUxaXD=='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
